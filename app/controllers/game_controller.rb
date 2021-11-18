24f2bc2afc9267daf9536c34d12339f532e42b0c
Troy Snowden <troybradsnowden@gmail.com>
2021-11-18 15:42:21 +0000

    add game controller and landing page and first screen

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,13 @@
+class GameController < ApplicationController
+  def index
+  end
+
+  def register_name
+    session[:user_name] = params[:name]
+    redirect_to "/game/home"
+  end
+
+  def home
+    @name = session[:user_name]
+  end
+end
