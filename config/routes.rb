1f815bbd767d0641bcf1a69c7e25f413de6217f9
Kirsty Harris <kittyharris5@yahoo.co.uk>
2021-11-18 11:07:38 +0000

    Added winner room and text

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get "/articles", to: "articles#index"
   get "/articles/room1", to: "articles#room1"
   get "/articles/room2", to: "articles#room2"
-  get "/articles/journey-end", to: "articles#journey_end"
+  get "/articles/journey-end", to: "articles#winner"
 
 end
