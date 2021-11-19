2b72d6294403b75769d2c1be2c490115cf0b27bc
Troy Snowden <troybradsnowden@gmail.com>
2021-11-19 14:04:52 +0000

    Add users table db migration and users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  
+  def register
+    @user = User.new
+  end
+
+  def create
+    session[:user_id] = User.create(user_params).id
+    redirect_to "/game/home"
+  end
+
+  def login
+    @user = User.new
+  end
+
+  def authenticate
+    redirect_after_login(User.find_by(username: params[:username]))
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to "/login"
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :full_name, :password, :profile_photo)
+  end
+
+  def redirect_after_login(user)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/posts'
+    else
+      redirect_to '/login'
+    end
+  end
+
+end
