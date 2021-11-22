6dbf9fc50f08886c77b3eb7c872218cf03f7c875
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-22 16:49:47 +0000

    BH_feat_08: Updated feature tests for navigation bar

--- spec/features/players_can_see_navigation_bar_spec.rb
+++ spec/features/players_can_see_navigation_bar_spec.rb
@@ -1,7 +1,15 @@
 RSpec.feature "Navigation Bar: ", type: :feature do
   
   scenario "players can see the navigation bar on the landing page" do
+    visit ('/')
     expect(page).to have_button 'How to play'
+    expect(page).to have_button 'Log in'
   end
+
+  # scenario "players can see the navigation bar on the register page" do
+  #   visit ('/register')
+  #   expect(page).to have_button 'How to play'
+  #   expect(page).to have_button 'Log in'
+  # end
 
 end
