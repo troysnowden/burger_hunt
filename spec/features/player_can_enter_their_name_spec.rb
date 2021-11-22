57e7e290b8fee522896acecda0df392f4e6f8bdb
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-22 10:17:33 +0000

    BH_test_02: Updated feature tests

--- spec/features/player_can_enter_their_name_spec.rb
+++ spec/features/player_can_enter_their_name_spec.rb
@@ -2,9 +2,9 @@ RSpec.feature "Player can register: ", type: :feature do
   
   scenario "players can enter their name on the register page" do
     visit ('/register')
-    expect(page).to have_content 'Enter your name'
-    expect(page).to have_button 'Begin'
-    click_button 'Begin'
+    expect(page).to have_content 'Username'
+    expect(page).to have_button 'Create User'
+    click_button 'Create User'
     expect(page).to have_current_path '/game/home'
   end
 
