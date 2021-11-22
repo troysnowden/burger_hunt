65bb11f6e67b9438bd285bde458b66955407c9f4
Troy Snowden <troybradsnowden@gmail.com>
2021-11-22 16:20:50 +0000

    Add in puzzle functionality

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,15 +7,26 @@ class GameController < ApplicationController
   end
 
   def page3
+    session[:puzzle_attempts] = 0 unless session[:puzzle_answer]
     @puzzle_text = "Puzzle Text"
     @puzzle_answer = session[:puzzle_answer] = "[puzzle-answer]"
-    session[:puzzle_attempts] = 0
+    if session[:correct_answer_found]
+      @correct_answer_found = true
+    end
+    if session[:puzzle_attempts] == 3
+      # add flash message here
+    end
   end
 
   def puzzle_check
-    if params[:puzzle_answer] == session[:puzzle_answer]
-      redirect_to "/game/page5"
+    if params[:puzzle_answer_input] == session[:puzzle_answer]
+      session[:puzzle_answer] = nil
+      # flash message to say correct answer maybe?
+      session[:correct_answer_found] = true
+      edirect_to "/game/page3"
     else
+      session[:correct_answer_found] = nil
+      session[:puzzle_attempts] += 1
       redirect_to "/game/page3"
     end
   end
