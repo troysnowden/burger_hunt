a32db24ca69ed54776080e7942289772efc3d3d2
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-23 16:05:58 +0000

    BH_feat_18: Updated feature test - needs session data to pass

--- spec/features/player_can_use_page_5_spec.rb
+++ spec/features/player_can_use_page_5_spec.rb
@@ -2,20 +2,32 @@ RSpec.feature "Page 5: ", type: :feature do
   
   scenario "player can see buttons on page 5" do
     visit ('game/page5')
-    expect(page).to have_link 'Wrong'
-    expect(page).to have_link 'Correct'
+    expect(page).to have_link 'Left (wrong)'
+    expect(page).to have_link 'Right (correct)'
   end
 
-  # scenario "player moves to page 6 when they click on the 'Wrong' button" do
+  # scenario "player moves to page 6 when they click on the 'Left (wrong)' button" do
   #   visit ('game/page5')
-  #   click_link 'Wrong' 
+  #   click_link 'Left (wrong)' 
   #   expect(page).to have_current_path '/game/page6'
   # end
 
-  # scenario "player moves to 'Win' when they click on the 'Correct' button" do
+  # scenario "player moves to 'Win' when they click on the 'Right (correct)' button" do
   #   visit ('game/page5')
-  #   click_link 'Correct' 
+  #   click_link 'Right (correct)' 
   #   expect(page).to have_current_path '/game/win'
   # end
+
+  scenario "player moves to page 6, then returns to page 5, and gets a hint about carrots" do
+    visit ('game/page5')
+    # expect(page).not_to have_text 'HINT' 
+    # click_link 'Left (wrong)'
+    # expect(page).to have_current_path '/game/page6'
+    # click_link 'Go back'
+    # expect(page).to have_current_path '/game/page5'
+    # expect(page.to have_text 'HINT')
+
+  end
+
 
 end
