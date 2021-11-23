fd34dc24928069f4518d81fb600046a434539f57
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-23 12:13:56 +0000

    BH_feat_17: Updated routes to use page5

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,14 @@ Rails.application.routes.draw do
   
   get "/game", to: "game#index"
 
-  get "/game/page-5", to: "game#page-5"
-  get "/game/home", to: "game#home"
-  get "/game/right", to: "game#right"
-  get "/game/left", to: "game#left"
-  get "/game/page2", to: "game#page2"
   get "/game/page1", to: "game#page1"
   post "/game/page1", to: "game#page2"
+  # The post route above may not be needed if we change the button to a link?
+  get "/game/page2", to: "game#page2"
   get "/game/page3", to: "game#page3"
   post "/game/puzzle-answer", to: "game#puzzle_check"
+
+  get "/game/page5", to: "game#page5"
 
   get "/register", to: "users#register"
   post "/register", to: "users#create"
