7dc05e713918b931d690baab1c714d2df3564169
Hassan Saleem <hassansaleem194@gmail.com>
2021-11-23 15:54:22 +0000

    added page 6 tests

--- spec/features/page_6_spec.rb
+++ spec/features/page_6_spec.rb
@@ -1,8 +1,14 @@
 RSpec.feature "Page 6", type: :feature do
-  scenario "Player can visit page 6 and dead end" do
+  scenario "Player can visit page 6 and reach a dead end" do
     visit ('/game/page6')
     expect(page).to have_content "Your instincts took you left… your instincts were wrong."
     expect(page).to have_link 'Question why a bicycle has 3 wheels...'
+  end
+
+  scenario "Player can visit page 6 and click the question button" do
+    visit ('/game/page6')
+    click_link 'Question why a bicycle has 3 wheels...'
+    expect(page).to have_content "The bike is the definition of being a third wheel."
   end
 
 end
