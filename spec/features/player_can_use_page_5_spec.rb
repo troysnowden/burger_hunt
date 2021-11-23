e3a2339bf7708b987da08aa62b14687985b311a6
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-23 12:15:44 +0000

    BH_feat_17: Updated feature test to use page5

--- spec/features/player_can_use_page_5_spec.rb
+++ spec/features/player_can_use_page_5_spec.rb
@@ -1,19 +1,19 @@
 RSpec.feature "Page 5: ", type: :feature do
   
   scenario "player can see buttons on page 5" do
-    visit ('game/page-5')
+    visit ('game/page5')
     expect(page).to have_button 'Wrong'
     expect(page).to have_button 'Correct'
   end
 
   # scenario "player moves to page 6 when they click on the 'Wrong' button" do
-  #   visit ('game/page-5')
+  #   visit ('game/page5')
   #   click_button 'Wrong' 
-  #   expect(page).to have_current_path '/game/page-6'
+  #   expect(page).to have_current_path '/game/page6'
   # end
 
   # scenario "player moves to 'Win' when they click on the 'Correct' button" do
-  #   visit ('game/page-5')
+  #   visit ('game/page5')
   #   click_button 'Correct' 
   #   expect(page).to have_current_path '/game/win'
   # end
