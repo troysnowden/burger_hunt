b97e6473b49c991a99c244081798d1fc8c2734fd
Kirsty Harris <kittyharris5@yahoo.co.uk>
2021-11-23 13:10:14 +0000

    Changed test back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   def redirect_after_login(user)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/game/page-1'
+      redirect_to '/game/home'
     else
       redirect_to '/login'
     end
