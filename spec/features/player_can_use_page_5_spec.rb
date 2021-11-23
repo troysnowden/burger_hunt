e1b810349a3b86b0b0728205079aedc7d9300e43
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-23 12:27:43 +0000

    BH_feat_17: Updated feature test to use links, updated erb to use links, added button class

--- spec/features/player_can_use_page_5_spec.rb
+++ spec/features/player_can_use_page_5_spec.rb
@@ -2,19 +2,19 @@ RSpec.feature "Page 5: ", type: :feature do
   
   scenario "player can see buttons on page 5" do
     visit ('game/page5')
-    expect(page).to have_button 'Wrong'
-    expect(page).to have_button 'Correct'
+    expect(page).to have_link 'Wrong'
+    expect(page).to have_link 'Correct'
   end
 
   # scenario "player moves to page 6 when they click on the 'Wrong' button" do
   #   visit ('game/page5')
-  #   click_button 'Wrong' 
+  #   click_link 'Wrong' 
   #   expect(page).to have_current_path '/game/page6'
   # end
 
   # scenario "player moves to 'Win' when they click on the 'Correct' button" do
   #   visit ('game/page5')
-  #   click_button 'Correct' 
+  #   click_link 'Correct' 
   #   expect(page).to have_current_path '/game/win'
   # end
 
