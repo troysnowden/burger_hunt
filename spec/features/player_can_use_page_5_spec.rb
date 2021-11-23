59b6f5871f64f83fa0b48e65138132d7ee6a5d1b
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-23 18:30:20 +0000

    BH_feat_18: updated feature tests

--- spec/features/player_can_use_page_5_spec.rb
+++ spec/features/player_can_use_page_5_spec.rb
@@ -1,16 +1,26 @@
 RSpec.feature "Page 5: ", type: :feature do
   
-  scenario "player can see buttons on page 5" do
+  scenario "player can see 'Left' button on page 5" do
     visit ('game/page5')
     expect(page).to have_link 'Left (wrong)'
-    expect(page).to have_link 'Right (correct)'
   end
 
-  # scenario "player moves to page 6 when they click on the 'Left (wrong)' button" do
-  #   visit ('game/page5')
-  #   click_link 'Left (wrong)' 
-  #   expect(page).to have_current_path '/game/page6'
-  # end
+  scenario "player can only see 'Right' button when they have eaten 3 carrots" do
+    visit ('game/page5')
+    expect(page).to have_link 'CARROT'
+    # click_link 'CARROT'
+    # click_link 'CARROT'
+    # click_link 'CARROT'
+    # click_link 'CARROT'
+    # expect(page).to have_link 'Right (correct)'
+  end
+
+
+  scenario "player moves to page 6 when they click on the 'Left (wrong)' button" do
+    visit ('game/page5')
+    click_link 'Left (wrong)' 
+    expect(page).to have_current_path '/game/page6'
+  end
 
   # scenario "player moves to 'Win' when they click on the 'Right (correct)' button" do
   #   visit ('game/page5')
@@ -20,14 +30,12 @@ RSpec.feature "Page 5: ", type: :feature do
 
   scenario "player moves to page 6, then returns to page 5, and gets a hint about carrots" do
     visit ('game/page5')
-    # expect(page).not_to have_text 'HINT' 
-    # click_link 'Left (wrong)'
-    # expect(page).to have_current_path '/game/page6'
-    # click_link 'Go back'
-    # expect(page).to have_current_path '/game/page5'
-    # expect(page.to have_text 'HINT')
-
+    expect(page).not_to have_text 'HINT' 
+    click_link 'Left (wrong)'
+    expect(page).to have_current_path '/game/page6'
+    click_link 'Go back'
+    expect(page).to have_current_path '/game/page5'
+    expect(page).to have_text 'HINT'
   end
-
 
 end
