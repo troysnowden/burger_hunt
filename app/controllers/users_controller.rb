934bf22a0a48bb1c4dc5c2413036a8313cbd6483
Kirsty Harris <kittyharris5@yahoo.co.uk>
2021-11-23 13:35:41 +0000

    Added required to register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-    params.require(:user).permit(:username,:password)
+    params.require(:user).permit(:username, :password)
   end
 
   def redirect_after_login(user)
