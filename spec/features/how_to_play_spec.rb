7ac309bc836de1eb388c22ae135ba8ba2db4395a
Hassan Saleem <hassansaleem194@gmail.com>
2021-11-25 10:12:10 +0000

    fixed typo in test

--- spec/features/how_to_play_spec.rb
+++ spec/features/how_to_play_spec.rb
@@ -2,6 +2,6 @@ RSpec.feature "How to play", type: :feature do
   scenario "Players can access the how to play modal" do
     visit ('/')
     click_button 'How to play'
-    expect(page).to have_content 'Here\'s how to play Buger Hunt'
+    expect(page).to have_content 'Here\'s how to play Burger Hunt'
   end
 end
