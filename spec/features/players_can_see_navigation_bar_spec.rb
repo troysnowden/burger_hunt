c3d86d346eaab923428673ebd4a1fc9ca8b1cf9e
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-25 08:59:29 +0000

    BH_feat_10: added feature test for Start again button, updated feature tests to look for links instead of buttons

--- spec/features/players_can_see_navigation_bar_spec.rb
+++ spec/features/players_can_see_navigation_bar_spec.rb
@@ -2,23 +2,23 @@ RSpec.feature "Navigation Bar: ", type: :feature do
   
   scenario "players can see the navigation bar on the landing page" do
     visit ('/')
-    expect(page).to have_button 'How to play'
-    expect(page).to have_button 'Log in'
+    expect(page).to have_link 'How to play'
+    expect(page).to have_link 'Log in'
   end
 
   scenario "players can see the navigation bar on the register page" do
     visit ('/register')
-    expect(page).to have_button 'How to play'
-    expect(page).to have_button 'Log in'
+    expect(page).to have_link 'How to play'
+    expect(page).to have_link 'Log in'
   end
 
   scenario "players can see the pocket button if logged in" do
     visit ('/register')
     fill_in "user_username", with: "johnsmith"
     fill_in "user_password", with: "password"
-    click_button "Create User"
-    expect(page).to have_button 'How to play'
-    expect(page).to_not have_button 'Log in'
+    click_button "Register"
+    expect(page).to have_link 'How to play'
+    expect(page).to_not have_link 'Log in'
     expect(page).to have_button 'Pocket'
     click_button "Pocket"
     expect(page).to have_content "Chocolate Bar - Equipped"
@@ -29,7 +29,7 @@ RSpec.feature "Navigation Bar: ", type: :feature do
     visit ('/register')
     fill_in "user_username", with: "johnsmith"
     fill_in "user_password", with: "password"
-    click_button "Create User"
+    click_button "Register"
     click_button "Pocket"
     click_button "Equip"
     expect(page).to_not have_content "Chocolate Bar - Equipped"
@@ -39,4 +39,13 @@ RSpec.feature "Navigation Bar: ", type: :feature do
     expect(page).to_not have_content "T-Rex Egg - Equipped"
   end
 
+  scenario "players can see the 'Start again' button if logged in" do
+    visit ('/register')
+    fill_in "user_username", with: "johnsmith"
+    fill_in "user_password", with: "password"
+    click_button "Register"
+    expect(page).to have_link 'How to play'
+    expect(page).to have_link 'Start again'
+    expect(page).to_not have_link 'Log in'
+  end
 end
